be5a3984fdda4250392b2f56694ec274d6f73746
Kevin Booth <kdbooth97@gmail.com>
2019-04-19 18:19:54 -0400

    Add player creation methods and data types

--- src/connectFour/Game.java
+++ src/connectFour/Game.java
@@ -2,13 +2,13 @@ package connectFour;
 
 import java.util.Scanner;
 
-import game.Board;
-
 public class Game {
 	
 	private Scanner scanner = new Scanner(System.in);
 	private int moves = 7 * 6;
 	private Board board;
+	private Player playerOne;
+	private Player playerTwo;
 	
 	public Game() {
 		board = new Board();
@@ -17,6 +17,26 @@ public class Game {
 	
 	private void startGame() {
 		printHeader();
+		createPlayerData();
+		
+	}
+	
+	private void createPlayerData() {
+		System.out.println("What is your name?: ");
+		String name = scanner.nextLine();
+		playerOne = new OrganicPlayer(name, "X");
+		playerTwo = new AutonomousPlayer("O");
+	}
+	
+	public int getMoves() { return moves; }
+	
+	public void decreaseMoves() { --moves; }
+	
+	public boolean checkMoves() {
+        if (moves == 0) {
+           return false;
+        }
+        return true;
 	}
 	
 	private void printHeader() {
