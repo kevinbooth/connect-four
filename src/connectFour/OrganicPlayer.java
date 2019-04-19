662ca16290f0c2492e58fd119cd1079f11771d79
Kevin Booth <kdbooth97@gmail.com>
2019-04-19 18:06:28 -0400

    Add OrganicPlayer class that implements Player

---
+++ src/connectFour/OrganicPlayer.java
@@ -0,0 +1,14 @@
+package connectFour;
+
+public class OrganicPlayer implements Player {
+	private String name;
+	private String tileType;
+	
+	public OrganicPlayer(String name, String tileType) {
+		
+	}
+	
+	public String getName() { return name; }
+	
+	public String getTileType() { return tileType; }
+}
