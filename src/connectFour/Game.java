c358bd7bf4ed9f7d37c946f95a0f2b3c6accccce
Kevin Booth <kdbooth97@gmail.com>
2019-04-19 17:55:14 -0400

    Add Game constructor, class data attributes, and startGame method

--- src/connectFour/Game.java
+++ src/connectFour/Game.java
@@ -1,5 +1,25 @@
 package connectFour;
 
-public class Game {
+import java.util.Scanner;
 
+import game.Board;
+
+public class Game {
+	
+	public static char[] number = new char[] { '1', '2' };
+	public static char[] badge = new char[] { 'X', 'O' };
+	private Scanner scanner = new Scanner(System.in);
+	private int moves = 7 * 6;
+	private int currentPlayer;
+	private Board board;
+	
+	public Game() {
+		currentPlayer = 1;
+		board = new Board();
+		startGame();
+	}
+	
+	private void startGame() {
+		
+	}
 }
