125e34cfaea38896fa641b7f6fcf7540d2fef928
Kevin Booth <kdbooth97@gmail.com>
2019-04-19 17:57:49 -0400

    Update Game data attributes and add printHeader function

--- src/connectFour/Game.java
+++ src/connectFour/Game.java
@@ -6,20 +6,24 @@ import game.Board;
 
 public class Game {
 	
-	public static char[] number = new char[] { '1', '2' };
-	public static char[] badge = new char[] { 'X', 'O' };
 	private Scanner scanner = new Scanner(System.in);
 	private int moves = 7 * 6;
-	private int currentPlayer;
 	private Board board;
 	
 	public Game() {
-		currentPlayer = 1;
 		board = new Board();
 		startGame();
 	}
 	
 	private void startGame() {
-		
+		printHeader();
+	}
+	
+	private void printHeader() {
+	    System.out.println("            CONNECT FOUR");
+	    System.out.println("-------------------------------------");
+	    System.out.println("Welcome to Connect Four!");
+	    System.out.println("Select a column number from 1 to 7:");
+	    System.out.println();
 	}
 }
