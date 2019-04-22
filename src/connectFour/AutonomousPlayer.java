8c0d3083c5f23606e114fded47f96778bfd7e870
Kevin Booth <kdbooth97@gmail.com>
2019-04-21 20:42:06 -0400

    Add column generation ability to AutonomousPlayer

--- src/connectFour/AutonomousPlayer.java
+++ src/connectFour/AutonomousPlayer.java
@@ -1,23 +1,30 @@
 package connectFour;
 
-import java.util.Random;
+import java.util.Collections;
+import java.util.List;
 
 public class AutonomousPlayer implements Player {
 	private String name = "Rob the Robot";
+	private int playerNumber;
 	private String tileType;
 	
-	public AutonomousPlayer(String tileType) {
+	public AutonomousPlayer(int playerNumber, String tileType) {
+		this.playerNumber = playerNumber;
 		this.tileType = tileType;
 	}
 	
 	public String getName() { return name; }
 	
+	public int getPlayerNumber() { return playerNumber; }
+	
 	public String getTileType() { return tileType; }
 	
-	public int chooseRandomColumn(int columnLimit) {
-		Random rand = new Random();
-		int column = rand.nextInt(columnLimit) + 1;
-		
-		return column;
+	public int chooseRandomColumn(List<Integer> columns) {
+		for (int column : columns) {
+			System.out.print(column + " ");
+		}
+		Collections.shuffle(columns);
+		System.out.println("Auto pick: " + columns.get(0));
+		return columns.get(0);
 	}
 }
