2b39bd038ea18f67f9e5237407eaab6e46a0e23f
Kevin Booth <kdbooth97@gmail.com>
2019-05-01 15:19:06 -0400

    Add rematch functionality to main method

--- src/connectFour/Main.java
+++ src/connectFour/Main.java
@@ -1,16 +1,38 @@
 package connectFour;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Game game;
-		boolean playing = true;
-		game = new Game();
-		game.startGame();
+		Scanner scanner = new Scanner(System.in);
+		boolean rematch = true;
 		
-		while(playing) {
-			// start player's turn
-			playing = game.toggleTurn();
+		while(rematch) {
+			Game game;
+			boolean playing = true;
+			game = new Game();
+			game.startGame();
+			
+			while(playing) {
+				playing = game.toggleTurn();
+			}
+			
+			while(true) {
+				System.out.print("Would you like a rematch? [y/N]: ");
+				String input = scanner.nextLine();
+				if (input.toLowerCase().equals("y")) {
+					System.out.println();
+					break;
+				} else if (input.toLowerCase().equals("n")) {
+					System.out.println("\nThanks for playing!");
+					scanner.close();
+					rematch = false;
+					break;
+				} else {
+					System.out.println("Your input was wrong.");
+				}
+			}
 		}
 	}
 
