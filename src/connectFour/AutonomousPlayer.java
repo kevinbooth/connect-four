47bcb3717142769ce794471c839ca6e1bd30ae80
Kevin Booth <kdbooth97@gmail.com>
2019-05-12 11:41:00 -0400

    Add difficulty to autonomous player

--- src/connectFour/AutonomousPlayer.java
+++ src/connectFour/AutonomousPlayer.java
@@ -2,15 +2,18 @@ package connectFour;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class AutonomousPlayer implements Player {
 	private String name = "Rob the Robot";
 	private int playerNumber;
 	private String tileType;
+	private int difficulty;
 	
-	public AutonomousPlayer(int playerNumber, String tileType) {
+	public AutonomousPlayer(int playerNumber, String tileType, int difficulty) {
 		this.playerNumber = playerNumber;
 		this.tileType = tileType;
+		this.difficulty = difficulty;
 	}
 	
 	public String getName() { return name; }
@@ -19,12 +22,29 @@ public class AutonomousPlayer implements Player {
 	
 	public String getTileType() { return tileType; }
 	
-	public int chooseRandomColumn(List<Integer> columns) {
+	public int chooseRandomColumn(List<Integer> columns, int bestMove) {
 		/*
 		 * Chooses a random column on the board based on
 		 * what columns have open spaces
 		 */
-		Collections.shuffle(columns);
-		return columns.get(0);
+		Random rand = new Random(); 
+		int odds;
+		
+		if (difficulty == 1) {
+			odds = rand.nextInt(2);
+		} else if (difficulty == 2) {
+			odds = rand.nextInt(1);
+		} else if (difficulty == 3) {
+			odds = 0;
+		} else {
+			odds = rand.nextInt(2);
+		}
+        
+		if (bestMove != -1 && odds == 0) {
+			return bestMove;
+		} else {
+			Collections.shuffle(columns);
+			return columns.get(0);
+		}
 	}
 }
